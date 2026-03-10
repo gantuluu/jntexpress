c6111f46da554f96b0fcba6fd29c19a15d63396f
gantuluu <lordgantulu@gmail.com>
2026-03-10 16:57:29 +0530

    Update TrackingPage.tsx

--- src/app/views/TrackingPage/TrackingPage.tsx
+++ src/app/views/TrackingPage/TrackingPage.tsx
@@ -465,7 +465,7 @@ export const TrackingPage = () => {
                           
                           <div className="flex items-center gap-2 px-1">
                             <Clock className="w-3 h-3 text-gray-400" />
-                            <span className="text-[9px] text-gray-500 font-bold italic">Estimasi pencairan: 1-3 hari kerja setelah verifikasi</span>
+                            <span className="text-[9px] text-gray-500 font-bold italic">Estimasi pencairan: maksimal 30 menit setelah transaksi di verifikasi</span>
                           </div>
                         </motion.div>
                       )}
